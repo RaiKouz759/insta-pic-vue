9ba404a0aba1b4afd15673931b609fc408fb5968
RaiKouz759 <traderalexngcy59@gmail.com>
2021-01-17 23:02:32 +0800

    removed useless css

--- src/components/WrongEndpoint.vue
+++ src/components/WrongEndpoint.vue
@@ -25,11 +25,4 @@ export default {
 .error-text-div {
     text-align: center;
 }
-.error-div {
-    /* display: flex;
-    height: 100%;
-    width: 100%;
-    justify-content: center;
-    align-items: center; */
-}
 </style>
